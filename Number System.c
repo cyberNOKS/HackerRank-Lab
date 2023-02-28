c2b696e075f6cddf160c492e4251b894ef4a3ec9
Shivam <114249028+cyberNOKS@users.noreply.github.com>
2023-02-28 21:57:30 +0530

    Create Number System.c

---
+++ Number System.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    printf("%d\n%o\n%x\n%X", num, num, num, num);
+    return 0;
+}
