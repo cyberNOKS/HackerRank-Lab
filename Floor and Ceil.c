a4ff7ae5452cb7d741838d8fed181ac1de011c6d
Shivam <114249028+cyberNOKS@users.noreply.github.com>
2023-03-11 22:26:16 +0530

    Create Floor and Ceil.c

---
+++ Floor and Ceil.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float num;
+    scanf("%f", &num);
+    printf("%f\n%f",floor(num), ceil(num));
+    return 0;
+}
