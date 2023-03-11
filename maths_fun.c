c0decf721ec13ca640b1d4e5dd1d1bd76f8f2394
Shivam <114249028+cyberNOKS@users.noreply.github.com>
2023-03-11 22:32:45 +0530

    Create maths_fun.c

---
+++ maths_fun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float n1,n2,n3;
+    scanf("%f%f%f",&n1,&n2,&n3);
+    printf("%f\n",pow(n1,n2));
+    printf("%f",sqrt(n3));
+    return 0;
+}
