97801049c2e8a8cd49e98500da13eae3fd9dd83b
Shivam <114249028+cyberNOKS@users.noreply.github.com>
2023-03-11 22:30:50 +0530

    Create Swapping 5.c

---
+++ Swapping 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n1,n2;
+    scanf("%d%d", &n1, &n2);
+    n1=n1^n2;
+    n2=n1^n2;
+    n1=n1^n2;
+    printf("%d %d",n1,n2);
+    return 0;
+}
